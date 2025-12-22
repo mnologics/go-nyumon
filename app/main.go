a148aee9a585b6bdca37b39526a7ba9269eb1f37
mno. <mnologics@gmail.com>
2025-12-22 14:33:59 +0900

    Station8完了

--- app/main.go
+++ app/main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func fetchUrl(url string) string {
@@ -36,12 +37,29 @@ func byteToJson(body []byte) string {
 }
 
 func main() {
-	/* 	問題
-	Dog API からランダムに画像 URL を取得し、その URL を出力するサブコマンドrandomを実装しましょう。
-	API のエンドポイントはGET https://dog.ceo/api/breeds/image/randomです。
+	// Station8
+
+	// 引数を受け取る処理を追加
+	/* オプション引数仕様
+	引数名はimagesとする。
+	例）dog-cli random --images 2
+	短縮系: -i
+	オプション引数が指定されていない場合やオプション引数のデフォルト値は1とする。
 	*/
-	url := "https://dog.ceo/api/breeds/image/random"
-	massage := fetchUrl(url)
-	fmt.Println(massage)
+	args := os.Args
+	images := 1
+	if len(args) >= 4 && (args[2] == "--images" || args[2] == "-i") {
+		fmt.Sscanf(args[3], "%d", &images)
+	}
+
+	for i := 0; i < images; i++ {
+		/* 	問題
+		Dog API からランダムに画像 URL を取得し、その URL を出力するサブコマンドrandomを実装しましょう。
+		API のエンドポイントはGET https://dog.ceo/api/breeds/image/randomです。
+		*/
+		url := "https://dog.ceo/api/breeds/image/random"
+		massage := fetchUrl(url)
+		fmt.Println(massage)
+	}
 
 }
