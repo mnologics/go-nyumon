25123860d3630ac7ece9fe335e740f030b4e3fdc
mno. <mnologics@gmail.com>
2025-12-22 14:22:58 +0900

    Station7完了

--- app/main.go
+++ app/main.go
@@ -3,8 +3,45 @@ Copyright © 2025 Masaki Noguchi <mnologics@gmail.com>
 */
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func fetchUrl(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	result := byteToJson(body)
+	return result
+}
+
+func byteToJson(body []byte) string {
+	var result map[string]interface{}
+	err := json.Unmarshal(body, &result)
+	if err != nil {
+		return ""
+	}
+	return result["message"].(string)
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	/* 	問題
+	Dog API からランダムに画像 URL を取得し、その URL を出力するサブコマンドrandomを実装しましょう。
+	API のエンドポイントはGET https://dog.ceo/api/breeds/image/randomです。
+	*/
+	url := "https://dog.ceo/api/breeds/image/random"
+	massage := fetchUrl(url)
+	fmt.Println(massage)
+
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.25.5
 
 require (
 	github.com/jstemmer/go-junit-report v1.0.0
+	github.com/spf13/cobra v1.10.2
 	github.com/stretchr/testify v1.11.1
+	golang.org/x/text v0.32.0
 	golang.org/x/tour v0.1.0
 )
 
@@ -12,7 +14,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
